2d95030ef037f4880399bead9bbf3b71b0cc6c32
May Matyi <may@matyi.net>
2023-05-31 14:38:10 -0700

    Refactor Observer, sync AVPlayer timeControlStatus

--- Sources/LiveViewNativeAVKit/VideoPlayerView.swift
+++ Sources/LiveViewNativeAVKit/VideoPlayerView.swift
@@ -4,30 +4,15 @@ import AVFoundation
 import Combine
 import LiveViewNative
 
-class VideoPlayerObserver: ObservableObject {
+class VideoPlayerObserver<R: RootRegistry>: ObservableObject {
     @Published var timeControlStatus: AVPlayer.TimeControlStatus = .waitingToPlayAtSpecifiedRate
+    @Published var timeControlStatusAsString: String = "waiting_to_play_at_specified_rate"
 
-    private var player: AVPlayer
+    var player: AVPlayer
     private var timeControlStatusObservation: AnyCancellable?
 
-    init(player: AVPlayer) {
-        self.player = player
-
-        observeTimeControlStatus()
-    }
-
-    private func observeTimeControlStatus() {
-        timeControlStatusObservation = player
-            .publisher(for: \.timeControlStatus)
-            .sink { [weak self] status in
-                self?.timeControlStatus = status
-
-                print("timeControlStatus updated: \(status.rawValue)")
-            }
-    }
-
-    deinit {
-        timeControlStatusObservation?.cancel()
+    init() {
+        self.player = AVPlayer()
     }
 }
 
@@ -35,39 +20,40 @@ struct VideoPlayerView<R: RootRegistry>: View {
     @ObservedElement private var element: ElementNode
     @LiveBinding(attribute: "time-control-status") private var timeControlStatus: String
     @LiveContext<R> private var context
-    @StateObject private var observer: VideoPlayerObserver
+    @StateObject private var observer: VideoPlayerObserver<R>
     var url: URL
-    private let player: AVPlayer
     
     init(element: ElementNode) {
-        let player = AVPlayer()
-
         if let urlAttr = element.attributeValue(for: "url") {
             self.url = URL(string: urlAttr)!
         } else {
             self.url = URL(string: "")!
         }
-        self.player = player
-        _observer = StateObject(wrappedValue: VideoPlayerObserver(player: player))
+        _observer = StateObject(wrappedValue: VideoPlayerObserver())
     }
 
     var body: some View {
-        VStack {
-            VideoPlayer(player: player)
-                .onAppear {
-                    testAction()
-                    player.replaceCurrentItem(with: AVPlayerItem(url: url))
-                }
-                .onReceive($element) {
-                    print("onReceive")
-                }
-            Button(action: testAction) {
-                Text("Set State")
+        VideoPlayer(player: self.observer.player)
+            .onAppear {
+                $timeControlStatus.wrappedValue = self.observer.timeControlStatusAsString
+
+                self.observer.player.replaceCurrentItem(with: AVPlayerItem(url: url))
             }
-        }
+            .onReceive(observer.player.publisher(for: \.timeControlStatus)) { status in
+                switch (status) {
+                case .paused:
+                    timeControlStatus = "paused"
+                case .playing:
+                    timeControlStatus = "playing"
+                case .waitingToPlayAtSpecifiedRate:
+                    timeControlStatus = "waiting_to_play_at_specified_rate"
+                @unknown default:
+                    timeControlStatus = "unknown"
+                }
+            }
     }
     
-    func testAction() {
-        $timeControlStatus.wrappedValue = "foo"
+    func pushTimeControlStatus(timeControlStatus: String) {
+        $timeControlStatus.wrappedValue = timeControlStatus
     }
 }
