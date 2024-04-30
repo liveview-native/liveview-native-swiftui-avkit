6e27dc9e91760b25be6fc64d12ce6f6a2ab5c83b
Carson Katri <Carson.katri@gmail.com>
2024-04-30 14:17:25 -0400

    Update to use the `@Addon` macro

--- Sources/LiveViewNativeAVKit/AVKitRegistry.swift
+++ Sources/LiveViewNativeAVKit/AVKitRegistry.swift
@@ -9,21 +9,26 @@ import LiveViewNative
 import LiveViewNativeStylesheet
 import SwiftUI
 
-/// The main LiveView Native registry for the LiveViewNativeAVKit add-on library.
-///
-/// Use this view in your LiveView view tree using the ``CustomRegistry`` (see <doc:AddCustomElement>).
-#if swift(>=5.8)
-@_documentation(visibility: public)
-#endif
-public struct AVKitRegistry<Root: RootRegistry>: CustomRegistry {
-    public enum TagName: String {
-        case videoPlayer = "VideoPlayer"
-    }
+public extension Addons {
+    public typealias AVKit = AvKit
     
-    public static func lookup(_ name: TagName, element: ElementNode) -> some View {
-        switch name {
-        case .videoPlayer:
-            VideoPlayer<Root>()
+    /// The main LiveView Native registry for the LiveViewNativeAVKit add-on library.
+    ///
+    /// Use this view in your LiveView view tree using the ``CustomRegistry`` (see <doc:AddCustomElement>).
+    #if swift(>=5.8)
+    @_documentation(visibility: public)
+    #endif
+    @Addon
+    public struct AvKit<Root: RootRegistry> {
+        public enum TagName: String {
+            case videoPlayer = "VideoPlayer"
+        }
+        
+        public static func lookup(_ name: TagName, element: ElementNode) -> some View {
+            switch name {
+            case .videoPlayer:
+                VideoPlayer<Root>()
+            }
         }
     }
 }
